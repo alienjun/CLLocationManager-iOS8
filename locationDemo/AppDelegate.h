185de68920d67882bed7bf8386b2d001990c36f1
alienjun <alienjun@126.com>
2014-09-21 15:11:10 +0800

    Initial Commit

---
+++ locationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  locationDemo
+//
+//  Created by AlienJun on 14-9-21.
+//  Copyright (c) 2014年 AlienJun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ locationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  locationDemo
+//
+//  Created by AlienJun on 14-9-21.
+//  Copyright (c) 2014年 AlienJun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
